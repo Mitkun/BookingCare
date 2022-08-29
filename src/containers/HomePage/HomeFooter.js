c69d0469a88bb13a163b78ef4745ccd093bb89fc
Mitkun <tcthien1991@gmail.com>
2022-08-29 21:49:32 +0700

    done home page

---
+++ src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+class HomeFooter extends Component {
+  render() {
+
+    return (
+      <>
+        <div className='home-footer'>
+          <p>
+            &copy; 2022 Mitkun. More infromation, please visit my Github.
+            <a href='https://www.google.com.vn' target='_blank' >&#8594; Click here &#8592;</a>
+          </p>
+        </div>
+      </>
+    );
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
